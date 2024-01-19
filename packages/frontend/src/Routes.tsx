2ff8b08461dd75e5da9b5e96f495361f6ccc7b77
turnboughsg <sophia.turnbough@outlook.com>
2024-01-19 11:20:48 -0600

    Add new files and dependencies for login functionality

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,10 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home.tsx";
+import NotFound from "./containers/NotFound.tsx";
+import Login from "./containers/Login.tsx";
 
 export default function Links() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            {/* Finally, catch all unmatched routes */}
+            <Route path="*" element={<NotFound />} />;
+            <Route path="/login" element={<Login />} />
         </Routes>
     );
 }
